f93b5521f02fb56507ec2ff7af428ec121600594
KwakYounHo <kwakyh951@gmail.com>
2023-06-02 09:39:02 +0900

    useState useEffect useRef 리액트 훅 사용해 보기 onClick props 사용해 보기

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Test from './test';
 
 const component:React.FC = ()=>{
+  const [count, setCount] = useState(0);
+  const renderCount = useRef(0)
+
+  useEffect(()=>{
+    renderCount.current++
+    console.log("렌더링 횟수", renderCount.current);
+  });
+
+  const renderingComponent:()=>void = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <h1>First Page</h1>
       <Test name={"이 문구가 나오면 성공입니다."}/>
+      <button onClick={renderingComponent}>렌더링</button>
     </div>
   )
 }
